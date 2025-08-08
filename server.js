827f2516abd7e8bdd83576161e0fa6edaf0c80d8
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-08-07 21:33:06 -0600

    Redirection to pages added

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
-import express from 'express';
+import express from "express";
+import connectDB from "./db/connection.js";
+import Participant from "./models/Participant.js";
+
+// VARIABLES
 const app = express();
 const router = express.Router();
 
 const PORT = 5500;
 
-app.get("/registrar", (req, res) => {
-    const token = req.query.token
-    res.json({token: token})
-})
+// VIEWS
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+// ROUTES AND CONTROLLERS
+app.get("/registrar", async (req, res) => {
+  const token = req.query.token;
+
+  try {
+
+    const participante = await Participant.findOne({ qrToken: token });
+
+    if (!participante) {
+        return res.redirect('/scan-fail.html')
+    }
+
+    if (participante.scanned) {
+        return res.redirect('/already-scanned.html'); 
+    }
+
+    participante.scanned = true;
+    await participante.save();
+    
+  } catch (error) {
+    console.error('Error confirming attendance:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+// START SERVER
+async function runServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`✅ App listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Connection to database failed:", error.message);
+    process.exit(1); 
+  }
+}
+
+runServer();
