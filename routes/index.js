791c9e8e1e562372b9df45ce71d424eff98fce1c
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-08-13 00:56:48 -0600

    Controller updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import Participant from "../models/Participant.js";
 import controller from "../controllers/index.js"
 
 const router = express.Router();
-
 
 router.get("/registrar", controller.registrarAsistencia);
 
