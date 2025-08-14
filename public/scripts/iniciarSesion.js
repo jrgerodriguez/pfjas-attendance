21d2187be9eff39a43d8001a9c416ed3621e2b27
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-08-13 22:19:57 -0600

    front end added

---
+++ public/scripts/iniciarSesion.js
@@ -0,0 +1,34 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  //Ningun campo vacio
+  if ([data.email, data.password].some((field) => field.trim() === "")) {
+    alert('Todos los campos son obligatorios.');
+    return;
+  }
+
+  try {
+    const response = await fetch("/iniciar-sesion", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+
+    const result = await response.json()
+
+    if(response.ok) {
+        window.location.href = '/sesion-iniciada.html'
+    } else {
+        alert(result.message);
+    }
+  } catch (error) {
+    alert('Network error. Please try again.')
+  }
+});
